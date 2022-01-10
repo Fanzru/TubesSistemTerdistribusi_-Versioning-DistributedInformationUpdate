6d5d28321236c6f46e2d05ad3647de878bc6ddc8
fanzru <fattahilaaf080701@gmail.com>
2022-01-10 20:43:44 +0700

    add command in line code

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import json
 
 # buat stub (proxy) untuk client
 server = xmlrpc.client.ServerProxy('http://localhost:8000')
-
+#untuk membaca dan meload database client yang disimpan pada file client.json
 f = open('client.json')
 data_client = json.load(f)
 
@@ -19,17 +19,25 @@ def print_pkg_manager():
 print('------------------------------------------------')
 print('----------- Update Package Managermu -----------')
 print('------------------------------------------------')
+# memamnggil prosedur print_pkg_manager()
 print_pkg_manager()
+# init value key
 key = ""
+
+#running client cli menu
 while key != "exit":
+    #input key command to update database
     key = input("comand : ")
+    # memanggil prosedur update() pada server rpc
     server.update(key)
     # lakukan pemanggilan fungsi querry() untuk mengetahui hasil persentase dari masing-masing kandidat
     querry_result = server.querry_result()
     data_server = querry_result
+    #handle menu key input
     if key == "mysql_update":
         mysql = data_server
         print("versi mysql : ", mysql)
+        #update database pada client.json
         data_client["mysql"] = mysql
         f = open("client.json", "w")
         json.dump(data_client, f)
@@ -37,6 +45,7 @@ while key != "exit":
     elif key == "mongodb_update":
         mongodb = data_server
         print("versi mongodb : ", mongodb)
+        #update database pada client.json
         data_client["mongodb"] = mongodb
         f = open("client.json", "w")
         json.dump(data_client, f)
@@ -49,4 +58,5 @@ while key != "exit":
     print('------------------------------------------------')
     print('----------------- Last Version -----------------')
     print_pkg_manager()
+#melakukan commit untuk menutup file client.json
 f.close()
